1890dc4ac70b4aa10d181f46bcba28f4d488203a
Bilal Husain <bilalh1690@gmail.com>
2022-05-16 00:14:14 +0530

    Delete Conversion of Decimal into other Number Systems.py

--- Conversion of Decimal into other Number Systems.py
+++
@@ -1,8 +0,0 @@
-#WPP to convert integer into other number systems
-int = 147
-
-print("The integer value of", int, "is:")
-print(bin(int), "in binary number system.")
-print(oct(int), "in octal number system.")
-print(hex(int), "in hexadecimal number system.")
-print(float(int), "in float number system.")
