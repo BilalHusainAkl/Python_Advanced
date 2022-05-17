2e7ebca2809aaa2e4c76171d9ff4a1c174e00e99
Bilal Husain <bilalh1690@gmail.com>
2022-05-17 11:30:11 +0530

    Delete Generating Pattern Type 10.py

--- Generating Pattern Type 10.py
+++
@@ -1,8 +0,0 @@
-#WPP to print patter #pattern10
-n=int(input("Enter number of rows: "))
-for i in range(n):
-    for j in range(n-i-1):
-        print(" ",end=" ")
-    for j in range(i+1):
-        print(n-j,end=" ")
-    print()
