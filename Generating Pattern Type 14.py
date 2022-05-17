0445d910138a455e6d4b22a558c8a2eed1d7fe28
Bilal Husain <bilalh1690@gmail.com>
2022-05-17 11:30:48 +0530

    Delete Generating Pattern Type 14.py

--- Generating Pattern Type 14.py
+++
@@ -1,8 +0,0 @@
-#WPP to print patter #pattern14
-n=int(input("Enter number of rows: "))
-for i in range(n):
-    for j in range(n-i-1):
-        print(" ",end="")
-    for j in range(i+1):
-        print(i+1,end=" ")
-    print()
